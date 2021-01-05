6f863c5ae0bc03e26bb8ff192e11aaa6b1f56b12
Carolain <Carolainsilva1@gmail.com>
2021-01-05 19:58:16 -0300

    agregue baner, cambie el estilo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { TheNav } from '../components/TheNav';
-import './style.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { TheNav } from "../components/TheNav";
+import "./style.css";
+import {useHistory} from 'react-router-dom';
 
-export const Login = ()=> {
+
+export const Login = () => {
+
+
+    const history = useHistory();
+
+const seeMore = ()=> {
     return(
-        <div>
-            <div className="container-nav">
-            <TheNav />
-
-            </div >
-            <div className="container-root">
-
-            <h2>BIENVENIDOS</h2>
-
-            <div className="container-btn">
-
-            <button className="btn btn-danger col-4">VER MAS</button>
-            </div>
-            </div>
-        </div>
+            history.push("/Main")
     )
+};
+
+
+  return (
+    <div>
+      <div className="container-nav">
+        <TheNav />
+      </div>
+      <div className="container-root">
+        <h2>BIENVENIDO 2021</h2>
+        <div className="container-btn">
+          <button onClick={seeMore} className="btn btn-warning col-5">VER CATALOGO</button>
+        </div>
+      </div>
+    </div>
+  );
 };
