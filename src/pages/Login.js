accc2540ba4a189e9145c41965e7040ec39aabf9
Carolain <Carolainsilva1@gmail.com>
2021-01-05 13:32:18 -0300

    termine prueba de react route

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export const Login = ()=> {
+    return(
+        <div>
+            <h2>Esto es LOGIN</h2>
+        </div>
+    )
+};
---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export const Main = ()=> {
+    return(
+        <div>
+            <h2>Esto es Maain</h2>
+        </div>
+    )
+}
