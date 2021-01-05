7f37146c37851320348ea4d5216c22b45c258fb6
Carolain <Carolainsilva1@gmail.com>
2021-01-05 14:35:00 -0300

    alinee con css sin usar margin de px

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { TheNav } from '../components/TheNav';
 import './style.css';
 
 export const Login = ()=> {
     return(
         <div>
-            <h2>Esto es LOGIN</h2>
+            <div className="container-nav">
+            <TheNav />
+
+            </div >
+            <div className="container-root">
+
+            <h2>BIENVENIDOS</h2>
+
+            <div className="container-btn">
+
+            <button className="btn btn-danger col-4">VER MAS</button>
+            </div>
+            </div>
         </div>
     )
 };
