0b189794a8b04cc9fe0e61e833d922c16aca41a6
Carolain <Carolainsilva1@gmail.com>
2021-01-19 01:17:31 -0300

    probando con onchange y input value

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
 import "./style.css";
 
@@ -5,18 +6,36 @@ export const Main = () => {
   //estado del render condicional
   const [login, setLogin] = useState(false);
   const [inputValue, setInputValue] = useState("");
+
+
 const handleInputChange = (e)=> {
   setInputValue(e.target.value);
   console.log(inputValue)
+  
+}
+
+const keyPress= (e)=> {
+  if (e.key === "Enter") {
+    console.log("presiono enter")
+   return true;
+
+  } else {
+    return false;
+  }
+
 }
 
   return (
     <div>{login ? <p>Tu estas loguead@</p> : <p>No estas Loguead@</p>}
     
 
-    <input  placeholder="escribe tu nombre"  className="input"  onChange={handleInputChange} ></input>
+    <input onKeyPress={keyPress}  placeholder="escribe tu nombre"  className="input"  onChange={handleInputChange} ></input>
+
+   <div className="onchange">
+   <h3>tu dices:</h3>
+   {inputValue}
    
-    
+   </div>
     </div>
   );
 };
