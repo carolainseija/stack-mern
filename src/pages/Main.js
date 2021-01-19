c698acae90d3e39ac1a048cbd24d60b1b71a501f
Carolain <Carolainsilva1@gmail.com>
2021-01-19 00:45:30 -0300

    evento onchange

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,8 +4,19 @@ import "./style.css";
 export const Main = () => {
   //estado del render condicional
   const [login, setLogin] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+const handleInputChange = (e)=> {
+  setInputValue(e.target.value);
+  console.log(inputValue)
+}
 
   return (
-    <div>{login ? <p>Tu estas loguead@</p> : <p>No estas Loguead@</p>}</div>
+    <div>{login ? <p>Tu estas loguead@</p> : <p>No estas Loguead@</p>}
+    
+
+    <input  placeholder="escribe tu nombre"  className="input"  onChange={handleInputChange} ></input>
+   
+    
+    </div>
   );
 };
